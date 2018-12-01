e6ee4355e2c221c6e1493aeb44b54198d45b3104
hashimjd <42841404+hashimjd@users.noreply.github.com>
2018-11-30 20:46:29 -0500

    Add files via upload

---
+++ Profitable.java
@@ -0,0 +1,14 @@
+/**
+ * <h1>Profitable</h1>
+ * 
+ * This interface represents something that can be used to make a profit. Along
+ * with returning total profits it must also be able to provide a report.
+ */
+public interface Profitable {
+
+
+    double getProfit();
+    String report();
+
+
+}
